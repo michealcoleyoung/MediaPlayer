2d89b73cb35cb17bf16ce017e048f8f61d5ac0a4
Your Name <coleyoungmusic@gmail.com>
2019-02-01 23:24:09 -0600

    added audio and modified design of player

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // TODO
 // Add toggle function for play and pause
 // Create function to handle slide bar for song
+
+
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    progressColor: 'purple'
+});
+
+wavesurfer.load('audio/BuildMeUp.wav')
