6321c72853d06f36e84eca6c6df7afb869a6c8bf
Your Name <coleyoungmusic@gmail.com>
2019-03-06 21:13:37 -0600

    added setTime function and added time tag on media player

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ function playAudio(id) {
         audio.play();
         audio.classList.toggle('pause');
         element.className = "fas fa-pause";
-        document.getElementById('progress').value = audio.currentTime;
-        console.log(audio.currentTime)
-        // document.getElementById('derp').innerHTML = audio.currentTime;
  
     }else{
         audio.pause();
@@ -21,9 +18,18 @@ function playAudio(id) {
     }
 }
 
+
+var aud = document.getElementById('song');
+aud.ontimeupdate = function() {setTime()};
+
+function setTime() {
+	var seconds = Math.floor(aud.currentTime % 60);	// converts milliseconds to seconds
+	var minutes = Math.floor(aud.currentTime / 60); // converts milliseconds to minutes
+	document.getElementById('time').innerHTML = `${minutes}:${seconds}`; // sets appropriate format of minutes:seconds 
+	document.getElementById('progress').value = aud.currentTime; // still needs work
+}
+
 // function setTrackProgress() {
 // 	// document.getElementById('progress').value = "100";
 // 	document.getElementById('derp').innerHTML = audio.currentTime;
-
-	
 // }
