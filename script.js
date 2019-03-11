e934b09b5cf1e1555537cf89d2146a98920bb579
Your Name <coleyoungmusic@gmail.com>
2019-03-10 19:59:05 -0500

    updated setTime function to account for the value and max of track

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// TODO
-// Add toggle function for play and pause
-// Create function to handle slide bar for song
-
+// toggles from play to pause
 function playAudio(id) {
 	// play and pause
 	var audio = document.getElementById(id);
@@ -29,16 +26,13 @@ function min_sec(num, size) {
 	return s. substr(s.length-size);
 }
 
-// sets time and progress
+// displays track time and sets the progress bar
 function setTime() {
 	var seconds = min_sec(Math.floor(aud.currentTime % 60), 2);	// converts milliseconds to seconds while calling min_sec function
     var minutes = Math.floor(aud.currentTime / 60); // converts milliseconds to minutes
     var duration = Math.floor(aud.duration);
 	document.getElementById('time').innerHTML = `<b>${minutes}:${seconds}/${parseInt(duration / 60, 10)}:${parseInt(duration % 60)}</b>`; // shows current time and full length of song
-	document.getElementById('progress').value = aud.currentTime / aud.duration; // sets track progress but still needs work
+    document.getElementById('progress').value = aud.currentTime;   // sets progress bar to current track time
+    document.getElementById('progress').max = aud.duration; // sets max track time value
 }
 
-// function setTrackProgress() {
-// 	// document.getElementById('progress').value = "100";
-// 	document.getElementById('derp').innerHTML = audio.currentTime;
-// }
