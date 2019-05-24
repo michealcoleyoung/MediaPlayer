790482f418a856cb0db5b36483018ac6548e24dd
Your Name <coleyoungmusic@gmail.com>
2019-05-23 20:09:35 -0500

    set ontimeupdate to window onload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function initializePlayer() {
+    let aud = document.getElementById('song');
+    let seekslider = document.getElementById('progress');
+    seekslider.addEventListener("change", audioSeek, false);
+    aud.addEventListener("timeupdate", seekUpdate, false);
+}
+
+window.onload = initializePlayer();
+
 // toggles from play to pause
 function playAudio(id) {
 	// play and pause
@@ -8,7 +17,9 @@ function playAudio(id) {
         audio.classList.toggle('pause');
         element.className = "fas fa-pause";
  
-    }else{
+    }
+    else
+    {
         audio.pause();
         // audio.currentTime = 0;
         element.className = "fas fa-play";
@@ -24,8 +35,7 @@ function min_sec(num, size) {
 let aud = document.getElementById('song');
 
 // creates a function expression that will occur automatically without being controlled by an event
-aud.ontimeupdate = function() {setTime()};
-
+window.onload = aud.ontimeupdate = function() {setTime()};
 
 // displays track time and sets the progress bar or slider
 function setTime() {
@@ -36,7 +46,6 @@ function setTime() {
     document.getElementById('progress').value = aud.currentTime;   // sets progress bar to current track time
     document.getElementById('progress').max = aud.duration; // sets max track time value
     let element = document.getElementById('play_pause');
-
     
     // once the song is complete the pause button switches back to play
     if (aud.currentTime == aud.duration) {
@@ -46,11 +55,15 @@ function setTime() {
 }
 
 
+let seekslider = document.getElementById('progress');
+
 function audioSeek() {
-    let slider = document.getElementById('progress');
-    let seekto = aud.duration * (slider.value / 100);
+    let seekto = aud.duration * (seekslider.value / 100);
     aud.currentTime = seekto;
-    var nt = aud.currentTime * (100 / aud.duration);
-    slider.value = nt;
-    // seeks current time from wherever the slider is located during the song
+}
+
+
+function seekUpdate() {
+    let newTime = aud.currentTime * (100 / aud.duration);
+    seekslider.value = newTime;
 }
